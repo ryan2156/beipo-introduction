4b1dc27c1b6d0da7e8e6f82dd532f000c0336c11
ryan2156 <adasdsd60112@gmail.com>
2020-12-08 20:15:53 +0800

    北埔介紹網頁的子網頁url

---
+++ mainmenu.php
@@ -0,0 +1,11 @@
+<?php require "header.php"; ?>
+
+<form action="culture.php" method="post">
+  <input type="submit" value="文化歷史"></p>
+</form>
+
+<form action="culture.php" method="post">
+  <input type="submit" value="景觀介紹"></p>
+</form>
+
+<?php require "footer.php"; ?>
