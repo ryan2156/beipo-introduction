364ca1afee463806825ada63c440a8c8f83c61f2
ryan2156 <adasdsd60112@gmail.com>
2020-12-23 20:09:15 +0800

    css 內部樣式 切割圖片

--- index.php
+++ index.php
@@ -1,24 +1,59 @@
-<?php require "header.php"; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>北普介紹網頁目錄</title>
+    <style>
+      body
+      {
+        background-color: #d0e4fe;
+      }
+      h1
+      {
+        color: orange;
+        text-align: center;
+      }
+      p
+      {
+        font-family: "Times New Roman";
+        font-size: 20px;
+      }
+      .img-demo
+      {
+        height:200px;
+        overflow:hidden;
+        border:1px solid #ccc;
+      }
 
-<img src="img-jpg/mainmenu1.jpg" width="1024"height="228">
-<form action="culture.php" method="post">
-  <input type="submit" value="文化歷史"></p>
-</form>
-
-<form action="sightseeing.php" method="post">
-  <input type="submit" value="景觀介紹"></p>
-</form>
-
-
-<form action="traffic.php" method="post">
-  <input type="submit" value="交通位置"></p>
-</form>
-
-<form action="industry.php" method="post">
-  <input type="submit" value="產業發展"></p>
-</form>
+      .img-demo img
+      {
+        margin-top: -5%;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="box">
+      <img src="img-jpg/mainmenu1.jpg" alt="北埔冷泉">
+    </div>
+    <h1>北普介紹~</h1>
 
 
+    <form action="culture.php" method="post">
+      <input type="submit" value="文化歷史"></p>
+    </form>
+
+    <form action="sightseeing.php" method="post">
+      <input type="submit" value="景觀介紹"></p>
+    </form>
 
 
-<?php require "footer.php"; ?>
+    <form action="traffic.php" method="post">
+      <input type="submit" value="交通位置"></p>
+    </form>
+
+    <form action="industry.php" method="post">
+      <input type="submit" value="產業發展"></p>
+    </form>
+
+  </body>
+<html>
