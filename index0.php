d4aa5869af70642331c51f998d408d9f70beb24f
ryan2156 <adasdsd60112@gmail.com>
2021-01-27 16:57:08 +0800

    圖片插入section跑版

--- index0.php
+++ index0.php
@@ -11,37 +11,53 @@
         color: rgb(255,255 ,255);
         position: relative;
       }
-      .box1{
-        margin: 5px 42%;
+      .section1{
+        padding: 7px;
+        margin: 5px auto;
         width: 25%;
         height: 90%;
         overflow: hidden;
+        font-family: '微軟正黑體';
+        text-align: left;
       }
-      nav{
+
+      .img{
+        max-height: 100%;
+        max-width: 100%;
+        width: auto;
+        height: auto;
+        position: relative;
+        top: 0;
+        left: 0;
+        margin: auto;
+        float: left;
+      }
+      header nav{
         width: 100%;
     		height: 80px;
     		background: rgba(0,0,0,.3);
     		position: absolute;
     		z-index: 10;
+        text-align: right;
     	}
-    	nav ul{
+    	header nav ul{
     		list-style: none;
     		margin: 10px;
     		padding: 0;
     	}
-    	nav ul li{
+    	header nav ul li{
     		text-align: center;
     		float: right;
     		line-height: 60px;
     	}
-    	nav ul li a{
+    	header nav ul li a{
     		width: 100px;
     		color: #fff;
     		font-size: 20px;
     		font-weight: 600;
     		display: block;
     	}
-    	nav ul li a:hover,nav ul li a:visited{
+    	header nav ul li a:hover,header nav ul li a:visited{
     		text-decoration: none;
     		background: rgba(0,0,0,.5);
     		color: #fff;
@@ -50,45 +66,37 @@
   			position: absolute;
   			top:50%;
   			left: 30%;}
-    	.imgw{position: relative;}
-      .family {
-        font-family: '微軟正黑體';
-      }
 
     </style>
   </head>
   <body>
     <header>
-      <div style="text-align:right";>
-        <nav>
-      		<ul>
-            <li><a href="#">柿餅機功能</a></li>
-            <li><a href="#">柿餅</a></li>
-            <li><a href="#">產品介紹</a></li>
-      			<li><a href="#">關於我們</a></li>
-            <li class="active"><a href="http://localhost/introduct/index0.php">首頁</a></li>
-            <li><h1>
-      		</ul>
-      	</nav>
-        <br>
-        <br>
-        <br>
-        <br>
-      </div>
+      <nav>
+    		<ul>
+          <li><a href="#">柿餅機功能</a></li>
+          <li><a href="#">柿餅</a></li>
+          <li><a href="#">產品介紹</a></li>
+    			<li><a href="#">關於我們</a></li>
+          <li class="active"><a href="http://localhost/introduct/index0.php">首頁</a></li>
+          <li><h1>
+    		</ul>
+    	</nav>
+      <br>
+      <br>
+      <br>
+      <br>
     </header>
     <article class="article-font">
-      <section class="box1">
-        <h2 style="font-family:'微軟正黑體'">自動曬柿餅機的可行性</h2>
-        <div class="family">
-          <p>
-            隨著北埔青壯年人口的外移加上當地人口老化，
-            人力資源極度匱乏，不足以應付柿餅產業所需
-            的勞動力，使得北埔的傳統文化逐漸式微。自動
-            曬柿餅機可以當成替代動力。
-          </p>
-        </div>
+      <section class="section1">
+        <img src="img-jpg/pic1.jpg" class="img">
+        <h2>自動曬柿餅機的可行性</h2>
+        <p>
+          隨著北埔青壯年人口的外移加上當地人口老化，
+          人力資源極度匱乏，不足以應付柿餅產業所需
+          的勞動力，使得北埔的傳統文化逐漸式微。自動
+          曬柿餅機可以當成替代動力。
+        </p>
       </section>
-
     </article>
     <footer>
     </footer>
