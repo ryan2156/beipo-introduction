6f22255f967163ce950ddb7a2f636a9935ecc95b
ryan2156 <adasdsd60112@gmail.com>
2021-02-04 15:55:16 +0800

    將第二版的版面控制好及上排選單的position改成fixed，內容尚未填。參考koa的設計。

---
+++ version2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> 北埔創生計畫 </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li id="top">&nbsp北埔社區創生計畫</li>
+          <li><a href="http://localhost/introduct/machine.php">柿餅機介紹</a></li>
+          <li><a href="#">柿餅</a></li>
+          <li><a href="#">關於我們</a></li>
+          <li><a href="http://localhost/introduct/version2.php">首頁</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section>
+      <div class="content">
+        <h2>計畫目的</h2>
+        <p>為改善北埔勞動人口的不足，製作的自動曬柿餅機將能改善這個問題。</p>
+      </div>
+    </section>
+    <section>
+      <div class="content">
+        2316
+      </div>
+    </section>
+    <section>
+      <div class="content">
+        78978416541651
+      </div>
+    </section>
+  </body>
+</html>
