741c94e123941186a66d7c14133580ea1d3c2657
ryan2156 <adasdsd60112@gmail.com>
2021-02-17 16:55:51 +0800

    設定top-box及內容增加

--- version2.php
+++ version2.php
@@ -2,13 +2,14 @@
 <html>
   <head>
     <title> 北埔創生計畫 </title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style0.css">
   </head>
   <body>
+    <span id="top-background-box"></span>
     <header>
       <nav>
         <ul>
-          <li id="top">&nbsp北埔社區創生計畫</li>
+          <li id="topicon">&nbsp北埔社區創生計畫</li>
           <li><a href="http://localhost/introduct/machine.php">柿餅機介紹</a></li>
           <li><a href="#">柿餅</a></li>
           <li><a href="#">關於我們</a></li>
@@ -16,20 +17,31 @@
         </ul>
       </nav>
     </header>
+    <section id="heading">
+      <div id="logo">北埔創生計畫</div>
+      <div id="tagline">將又老又窮的地方重新創生</div>
+    </section>
     <section>
+      <hr>
       <div class="content">
-        <h2>計畫目的</h2>
-        <p>為改善北埔勞動人口的不足，製作的自動曬柿餅機將能改善這個問題。</p>
+        <h2 class="h2title">計畫目的</h2>
+        <p>為改善北埔勞動人口的不足，本團隊將設計自動曬柿餅機已達到彌補人力不足及實現地
+          方創生。
+        </p>
+
       </div>
     </section>
     <section>
       <div class="content">
-        2316
+        <h2 class="h2title">北埔特色</h2>
+        <p>
+          在北埔每年9到11月為曬柿餅重要的傳統活動之一，可是隨著城鄉發展差距，勞動力人口的流失使得北埔柿餅產業沒落。
+        </p>
       </div>
     </section>
     <section>
       <div class="content">
-        78978416541651
+        <h2 class="h2title">實現方法</h2>
       </div>
     </section>
   </body>
