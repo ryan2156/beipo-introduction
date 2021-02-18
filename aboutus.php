b41c01d416a3045f6d169b9cfb06deadb04664f9
ryan2156 <adasdsd60112@gmail.com>
2021-02-18 16:56:04 +0800

    網頁首頁背景圖片的滿版，子網頁的創作動機。

--- aboutus.php
+++ aboutus.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="zh">
   <head>
     <meta charset="utf-8">
+    <title>關於我們</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
@@ -16,19 +17,5 @@
         </ul>
       </nav>
     </header>  <!--上排選單-->
-    <section>
-      <div class="content">
-        <h2>創作動機</h2>
-        <pre id="motivation">
-          對於北埔當地傳統柿餅文化的流失
-          原有的柿餅產業本來就已經很式微了
-          加上年輕一輩的勞動力外流讓傳統客家文化的傳承更難上加難
-          因此我們想要透過物聯網iot技術配合自動化機械來加以挽回傳統產業的逝去
-          並添加文化創新和技術改進
-          讓曾經的柿餅產業容光煥發
-        </pre>
-      </div>
-    </section>
-    <section
   </body>
-<html>
+</html>
--- version2.php
+++ version2.php
@@ -2,46 +2,56 @@
 <html>
   <head>
     <title> 北埔創生計畫 </title>
-    <link rel="stylesheet" type="text/css" href="css/style0.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
-    <span id="top-background-box"></span>
-    <header>
+    <span id="top-background-box"></span> <!--背景底圖-->
+    <header>  <!--上排選單-->
       <nav>
         <ul>
-          <li id="topicon">&nbsp北埔社區創生計畫</li>
+          <li id="topicon">&nbsp維大力團隊</li>
           <li><a href="http://localhost/introduct/machine.php">柿餅機介紹</a></li>
           <li><a href="#">柿餅</a></li>
-          <li><a href="#">關於我們</a></li>
+          <li><a href="http://localhost/introduct/aboutus.php">關於我們</a></li>
           <li><a href="http://localhost/introduct/version2.php">首頁</a></li>
         </ul>
       </nav>
-    </header>
-    <section id="heading">
+    </header>  <!--上排選單-->
+    <section id="heading">  <!--大標題-->
       <div id="logo">北埔創生計畫</div>
       <div id="tagline">將又老又窮的地方重新創生</div>
     </section>
-    <section>
+    <section>  <!--計畫目的-->
       <hr>
       <div class="content">
-        <h2 class="h2title">計畫目的</h2>
-        <p>為改善北埔勞動人口的不足，本團隊將設計自動曬柿餅機已達到彌補人力不足及實現地
-          方創生。
+        <h2>計畫目的</h2>
+        <p>為改善北埔勞動人口的不足，本團隊將設計自動曬柿餅機已達到彌補人力不足及實現地方創生。
         </p>
-
       </div>
     </section>
-    <section>
+    <section>  <!--北埔特色及問題-->
       <div class="content">
-        <h2 class="h2title">北埔特色</h2>
+        <h2>北埔特色</h2>
         <p>
-          在北埔每年9到11月為曬柿餅重要的傳統活動之一，可是隨著城鄉發展差距，勞動力人口的流失使得北埔柿餅產業沒落。
+          北埔老街位在臺灣新竹縣北埔鄉，其範圍包括北埔街、廟前街、南興街、城門街，
+          街區內有金廣福公館、天水堂、北埔慈天宮等古蹟，
+          為新竹縣的著名旅遊景點。
+          其特產有靠秋季九降風製成的柿餅、柿乾以及芋仔番薯月餅，
+          而另稱「膨風茶」的東方美人茶與客家擂茶亦頗有名氣。
         </p>
       </div>
     </section>
-    <section>
+    <section>  <!--實現方法-->
       <div class="content">
-        <h2 class="h2title">實現方法</h2>
+        <h2>實現方法</h2>
+        <p>
+          本團隊研發一種機器名為自動曬柿餅機，將能取代曬柿餅的搬運工作，其功能含有
+        </p>
+        <ul>
+          <li>移動功能</li>
+          <li>自動開架功能</li>
+          <li>感測當地溫溼度功能</li>
+        </ul>
       </div>
     </section>
   </body>
